340e36f6f99f5734e6590f9905bde3f72ab703ab
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-05 22:59:20 -0600

    Iteration 1 commit

---
+++ lib/car.rb
@@ -0,0 +1,27 @@
+class Car 
+    attr_reader :make, :model, :make_model, :monthly_payment, :loan_length, :color
+
+    def initialize(make_model, monthly_payment, loan_length)
+        make_model = make_model.split(" ")
+        @make = make_model[0]
+        @model = make_model[1]
+        @monthly_payment = monthly_payment
+        @loan_length = loan_length
+        @color = nil
+    end
+
+    def total_cost
+        monthly_payment * loan_length
+    end
+
+    # def paint(color)
+    #     @color = color
+    # end
+
+    def paint!(color)
+        @color = color
+    end
+
+    
+
+end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -34,7 +34,17 @@ RSpec.describe Car do
 
   it 'has a total cost' do
     car = Car.new("Ford Mustang", 1500, 36)
-
+    
     expect(car.total_cost).to eq(54000)
+  end
+  
+  it 'has no color by default, but can be painted' do
+    car = Car.new("Ford Mustang", 1500, 36)
+  
+    expect(car.color).to eq nil
+
+    car.paint!(:blue)
+
+    expect(car.color).to eq :blue
   end
 end
